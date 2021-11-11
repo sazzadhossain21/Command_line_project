7b805309f256d29db363662e1fe11052dc3f6d42
sazzadhossain21 <compus1983@gmail.com>
2021-11-10 19:28:42 -0500

    this is git bash project

---
+++ src/main/java/com/pagefactory/MasterPageFactory.java
@@ -0,0 +1,14 @@
+package com.pagefactory;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class MasterPageFactory {
+@FindBy(xpath="//*[@class='Element-sc-1dc3ws0-0 Link__StyledLink-sc-17puyxl-0 eklGNJ giYPMc atom-link'])[1]")
+private WebElement spendsavebtn;
+@FindBy(xpath="")
+WebElement getaspirationbtn;
+@FindBy(xpath="")
+WebElement email;
+
+}
